6a96a3ce34c95728b15875eedfafaec7fec446fa
Ales Manuel Papacek <ales.papacek@seznam.cz>
2025-04-02 16:37:21 +0200

    Add script to calculate inter annotator agreement
    Calculate fscore, percentage of exact match etymology for morphs. Write
    differences to file

---
+++ inter_annotator.py
@@ -0,0 +1,22 @@
+from utils import load_annotations,calculate_cohen_kappa,evaluate
+
+# Set file paths.
+dev_file_ales = 'annotations/dev.tsv'
+dev_file_tomas = 'annotations/dev_annotator2.tsv'
+differences_file = 'annotator_differences.tsv'
+
+# Load annotated test sentences (target data).
+dev_sentences_annotator1_ales = load_annotations(dev_file_ales)
+
+dev_sentences_annotator2 = load_annotations(dev_file_tomas)
+
+
+annotator1 = dev_sentences_annotator2
+annotator2 = dev_sentences_annotator1_ales
+f_score, exact_match = evaluate(annotator2,annotator1,differences_file)
+
+print("----- Inter-Annotator Agreement -----")
+print(f"F-score {f_score:.3f} %, exact_match {exact_match:.3f} %")
+
+cohen_kappa = calculate_cohen_kappa(annotator1,annotator2)
+print(f"Coehns kappa: {cohen_kappa:.3f}")
