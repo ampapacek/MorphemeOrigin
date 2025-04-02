0dfd1035cacf1cbd82606cd9a48c08a46e19402f
Ales Manuel Papacek <ales.papacek@seznam.cz>
2025-04-02 14:49:20 +0200

    Add parent Model class

---
+++ model.py
@@ -0,0 +1,43 @@
+class Model:
+    """
+    A parent class for models.
+    
+    This class provides a template for model implementations with a name,
+    and methods fit and predict that should be overridden in subclasses.
+    """
+    
+    def __init__(self, name: str = "Model"):
+        """
+        Initialize the model with a name.
+        
+        Args:
+            name (str): Name of the model. Defaults to "Model".
+        """
+        self.name = name
+
+    def fit(self, data):
+        """
+        Fit the model to the training data.
+        
+        Args:
+            data: Training data used to fit the model.
+        """
+        raise NotImplementedError("The fit() method must be implemented by subclasses.")
+
+    def predict(self, data):
+        """
+        Predict output using the model based on new data.
+        
+        Args:
+            data: New data for which predictions are to be made.
+        
+        Returns:
+            Predictions based on the model.
+        """
+        raise NotImplementedError("The predict() method must be implemented by subclasses.")
+
+    def __str__(self) -> str:
+        """
+        Return a string representation of the model.
+        """
+        return f"{self.__class__.__name__} (name={self.name})"
